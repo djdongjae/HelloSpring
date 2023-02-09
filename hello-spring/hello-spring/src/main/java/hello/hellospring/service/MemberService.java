ca08f8a419a997a8888d69869f0e25b3050c250d
Dong-Jae Oh <isf1999@naver.com>
2023-02-09 18:01:08 +0900

    modify_@Component를 통한 컴포넌트 스캔과 자동 의존관계 설정

--- hello-spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -3,14 +3,18 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
     //Test 파일 자동 생성 : ctrl + shift + T
     
     MemberRepository memberRepository;
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
@@ -19,6 +23,7 @@ public class MemberService {
     회원가입
      */
     public Long join(Member member) {
+        validateDuplicateMember(member);
         memberRepository.save(member);
         return member.getId();
     }
